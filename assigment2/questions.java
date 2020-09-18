2f674a479b0451d9260bfc73c747cc9f460860cf
NuriYildiz <NuriYildiz@.github.com>
2020-09-18 14:09:18 +0300

    Create questions.java

---
+++ assigment2/questions.java
@@ -0,0 +1,74 @@
+package assigment2;
+
+public class questions {
+	//question 1
+	
+	public static void main(String[] args) {
+		int count = 20;
+	System.out.println("value of the count =" + count);
+	
+	//question 2
+	
+	String message = "hello world";
+	System.out.println(message);
+	
+	//question 3
+	
+	int num = 10;
+	double cost =20;
+	System.out.println(num + " "+ cost);
+	
+	
+	//question 4
+	
+	String word = "Be Happy";	
+	System.out.println("Today's Word-Of-The-Day is: " + word);
+	
+	//question 5
+	
+	String first = "you";
+	String second = "me";
+	System.out.println("first is " + first + " second= " + second );
+	
+	//question 6
+	
+	String a = "Bob";
+	String b = "James";
+	String c = "Bet";
+	String d = "Herb";
+	String e = "James";
+	
+	System.out.println(a + ", " + b + ", " + c + ", " + d +  ", " + e + ", ");
+	
+	
+	//question 7
+	
+	int i = 5;
+	float f = 14.5f;
+	System.out.println("i=" + i +" " + "f=" + f);
+	
+	//question 8
+	
+	String lan1 = "java";
+	String lan2 = "SQL";
+	
+	System.out.println("I will learn \""+ lan1 + "\" and \"" + lan2 + "\" at CybertekSchool." );
+	
+	//question 9
+	
+	byte steps = 100;
+	short miles = 5000;
+	int count1 = 1_000_000;	
+	long population = 3434455667L;
+			
+	//question 10 (There is another "second" up there in question 5. That's why I have named as second1)
+		
+	int hour = 10;
+	int minute = 35;
+	int second1 = 55;
+	String amOrPm = "PM";
+	System.out.println(hour + ":" + minute + ":" + second1 + " " + amOrPm);
+	
+	}
+
+}
